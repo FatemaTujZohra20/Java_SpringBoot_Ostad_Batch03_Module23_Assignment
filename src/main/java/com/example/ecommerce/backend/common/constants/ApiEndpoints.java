31988ffdaa2f2d248d696ec34edfd1c5d7b92d69
Pial Kanti Samadder <pialkanti2012@gmail.com>
2026-04-26 01:25:42 +0600

    Added entities and related flyway migration scripts

---
+++ src/main/java/com/example/ecommerce/backend/common/constants/ApiEndpoints.java
@@ -0,0 +1,9 @@
+package com.example.ecommerce.backend.common.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiEndpoints {
+    private static final String API_VERSION = "/api/v1";
+}
